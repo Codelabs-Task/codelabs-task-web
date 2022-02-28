ffa36537fbdea1d5ea13ae5a6071cf1e8e8462f7
admin <55054691+deweks0@users.noreply.github.com>
2022-02-28 18:52:31 +0700

    make elements

--- js/components/CardComponent.js
+++ js/components/CardComponent.js
@@ -1,28 +1,6 @@
 const CardComponent = (data, container) => {
 	const render = (data) => {
-		element = `
-          <div class="m-1 cursor-pointer" onclick="navigateToDetail(${
-			data.id
-		})">
-                         <div class="border-radius-pill shadow inline-block">
-                              <div class="">
-                                   <img src="https://image.tmdb.org/t/p/w500/${
-								data.poster_path
-							}" class="w-small-image border-radius-pill-top" alt="">
-                              </div>
-                              <div class="border-radius-pill-bottom py-1 px-1">
-                                   <h3>${
-								data.title.length > 18
-									? data.title.slice(0, 18) + "..."
-									: data.title
-							}</h3>  (${new Date(
-			data.release_date
-		).getFullYear()})</h5>
-                                   <h5>${data.category}</h5>
-          
-                              </div>
-                         </div>
-                    </div>`;
+		element = `<card-component id="${data.id}" image="${data.poster_path}" title="${data.title}" year="${data.release_date}"></card-component>`;
 		return element;
 	};
 	const setAll = () => {
